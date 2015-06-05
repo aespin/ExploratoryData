e5931cca5366f89b1d3da854ff473537f67621c4
aespin <a.espin@maastrichtuniversity.nl>
2015-06-05 15:39:52 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,5 +6,4 @@ electricity <- read.table("household_power_consumption.txt", header = T, sep=";"
 dataplott<- subset(electricity, electricity$Date=="1/2/2007"|electricity$Date=="2/2/2007") 
 #Plot
 library("ggplot2")
-qplot <-  ggplot(data=dataplott, aes(x=dataplott$Time, y=dataplott$Global_active_power, colour=black))
-qplot<-qplot+geom_line()
+
